6a876b84ff882f6d4e7fc3ace35787311fb4746d
jrusev <jrusev@gmail.com>
2015-06-05 03:32:32 +0300

    Solve 50, 51, 52, 53

--- src/fourclojure_solutions/core.clj
+++ src/fourclojure_solutions/core.clj
@@ -276,3 +276,49 @@ mapcat list
 ;; (= (__ 3 [1 2 3 4 5 6]) [[1 2 3] [4 5 6]])
 #(vector (take % %2) (drop % %2))
 (juxt take drop)
+
+;; 50. Split by Type
+;; Write a function which takes a sequence consisting of items with
+;; different types and splits them up into a set of homogeneous
+;; sub-sequences. The internal order of each sub-sequence should be
+;; maintained, but the sub-sequences themselves can be returned in any
+;; order (this is why 'set' is used in thetest cases).
+;; (= (set (__ [1 :a 2 :b 3 :c])) #{[1 2 3] [:a :b :c]})
+#(vals (group-by type %))
+
+;; 51. Advanced Destructuring
+;; Here is an example of some more sophisticated destructuring.
+;; (= [1 2 [3 4 5] [1 2 3 4 5]] (let [[a b & c :as d] __] [a b c d]))
+[1 2 3 4 5]
+
+;; 52. Intro to Destructuring
+;; Let bindings and function parameter lists support destructuring.
+;; (= [2 4] (let [[a b c d e f g] (range)] __))
+[c e]
+
+;; 53. Longest Increasing Sub-Seq
+;; Given a vector of integers, find the longest consecutive
+;; sub-sequence of increasing numbers. If two sub-sequences have the
+;; same length, use the one that occurs first. An increasing
+;; sub-sequence must have a length of 2 or greater to qualify.
+;; (= (__ [1 0 1 2 3 0 4 5]) [0 1 2 3])
+(fn [coll]
+  (loop [[x & xs] coll
+         curr [x]
+         longest [x]]
+    (if-not (seq? xs)
+      (if (second longest) longest [])
+      (let [y (first xs)
+            curr (if (< x y) (conj curr y) [y])
+            longest (max-key count curr longest)]
+        (recur xs curr longest)))))
+
+(fn [coll]
+  (->>
+    (range 2 (inc (count coll)))
+    (mapcat #(partition % 1 coll))
+    (filter #(apply < %))
+    (cons [])
+    (sort-by count >)
+     first))
+
