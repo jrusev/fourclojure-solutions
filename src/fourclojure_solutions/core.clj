61d5842f0bdfe075fcdabc3264a60a61c78ba448
jrusev <jrusev@gmail.com>
2015-06-16 00:40:25 +0300

    Update description to 59

--- src/fourclojure_solutions/core.clj
+++ src/fourclojure_solutions/core.clj
@@ -409,8 +409,7 @@ reduce #(if ((set %) %2) % (conj % %2)) []
 ;; 59. Juxtaposition
 ;; Take a set of functions and return a new function that takes a
 ;; variable number of arguments and returns a sequence containing the
-;; result of applying each function left-to-right to the argument
-;; list.
+;; result of applying each function left-to-right to the argument list.
 ;; Special Restrictions: juxt
 ;; (fn [& funcs] (fn [& args] (map #(apply % args) funcs)))
 (fn [& fs]
