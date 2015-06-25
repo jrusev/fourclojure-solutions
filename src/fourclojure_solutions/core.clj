b0718835e09dcb39592f4bc2e93ff70827f7c896
jrusev <jrusev@gmail.com>
2015-06-25 19:01:01 +0300

    Solve 77

--- src/fourclojure_solutions/core.clj
+++ src/fourclojure_solutions/core.clj
@@ -636,3 +636,24 @@ apply (fn [f i & xs] ((fn ff [] (lazy-cat [i] (map f (ff) xs)))))
           (filter
            #(= 1 (.gcd (biginteger %) (biginteger n)))
            (range 2 n)))))
+
+;; 77. Anagram Finder
+;; Write a function which finds all the anagrams in a vector of words.
+;; A word x is an anagram of word y if all the letters in x can be
+;; rearranged in a different order to form y. Your function should
+;; return a set of sets, where each sub-set is a group of words which
+;; are anagrams of each other. Each sub-set should have at least two
+;; words. Words without any anagrams should not be included in the
+;; result.
+(fn [xs]
+  (->> (group-by sort xs)
+       (vals)
+       (filter #(> (count %) 1))
+       (map set)
+       (set)))
+
+#(set (for [[_ g] (group-by frequencies %)
+            :when (next g)]
+        (set g)))
+
+#(->> % (group-by set) vals (filter next) (map set) set)
