e02ec7f14ebaa883138ab5865738c1c05e4930ac
jrusev <jrusev@gmail.com>
2015-07-21 23:22:38 +0300

    Solve 98

--- src/fourclojure_solutions/core.clj
+++ src/fourclojure_solutions/core.clj
@@ -1231,3 +1231,14 @@ reduce #((if (% %2) disj conj) % %2)
 ;; youz's solution
 (fn [n] (-> (iterate #(map + `[0 ~@%] `[~@% 0]) [1])
             (nth (dec n))))
+
+;; 98. Equivalence Classes
+;; A function f defined on a domain D induces an equivalence relation
+;; on D, as follows: a is equivalent to b with respect to f if and
+;; only if (f a) is equal to (f b). Write a function with arguments f
+;; and D that computes the equivalence classes of D with respect to f.
+;; (= (__ #(* % %) #{-2 -1 0 1 2})
+;;    #{#{0} #{1 -1} #{2 -2}})
+(fn [f d]
+  (set (map set (vals (group-by f d)))))
+
